c714caa44d6f15de154b61526d6b85530b5697ff
kdubasov <cergocergo41@gmail.com>
2022-11-03 12:28:59 +0300

    add link to Bridge in NavbarTop.jsx

--- client/src/general-components/NavbarTop/NavbarTop.jsx
+++ client/src/general-components/NavbarTop/NavbarTop.jsx
@@ -5,6 +5,7 @@ import {Navbar} from "react-bootstrap";
 import {useState} from "react";
 import ModalLogin from "../ModalLogin/ModalLogin";
 import {useWeb3React} from "@web3-react/core";
+import {Link} from "react-router-dom";
 
 const NavbarTop = () => {
 
@@ -26,27 +27,39 @@ const NavbarTop = () => {
                     </span>
                 </div>
 
-                {
-                    active && chainId===40272 ?
-                        <button onClick={() => setShowModalLogin(!showModalLogin)} className={`connect success`}>
-                            {
-                                !showModalLogin?
-                                    <>
-                                        <img src="/images/general/success.svg" alt=""/>
-                                        Connected
-                                    </>:'Close'
-                            }
-                        </button>:
-                        <button onClick={() => setShowModalLogin(!showModalLogin)} className={`connect`}>
-                            {
-                                !showModalLogin?
-                                    <>
-                                        <img src="/images/general/user.svg" alt=""/>
-                                        <span>Connect</span> Account
-                                    </>:'Close'
-                            }
-                        </button>
-                }
+                <div className={"d-flex"}>
+
+                    <Link
+                        to={'/bridge'}
+                        target={"_blank"}
+                        rel={"noreferrer"}
+                        className={"bridgeLink"}
+                    >
+                        Go to Bridge
+                    </Link>
+
+                    {
+                        active && chainId===40272 ?
+                            <button onClick={() => setShowModalLogin(!showModalLogin)} className={`connect success`}>
+                                {
+                                    !showModalLogin?
+                                        <>
+                                            <img src="/images/general/success.svg" alt=""/>
+                                            Connected
+                                        </>:'Close'
+                                }
+                            </button>:
+                            <button onClick={() => setShowModalLogin(!showModalLogin)} className={`connect`}>
+                                {
+                                    !showModalLogin?
+                                        <>
+                                            <img src="/images/general/user.svg" alt=""/>
+                                            <span>Connect</span> Account
+                                        </>:'Close'
+                                }
+                            </button>
+                    }
+                </div>
             </Navbar>
 
             <ModalLogin show={showModalLogin} onHide={() => setShowModalLogin(false)} />
