9a452b33ff15ce9e98bf64d1d07e2d62e7084ab8
integ <cergocergo41@gmail.com>
2022-11-07 15:55:14 +0300

    version with back php

--- client/src/functions/getStrAfterDot.js
+++
@@ -1,7 +0,0 @@
-export const getStrAfterDot = str => {
-    if (String(str).indexOf('.') !== -1){
-        return String(str).slice(0,String(str).indexOf('.') + 3);
-    }else {
-        return str
-    }
-}
--- server/requests.js
+++
@@ -1,8 +0,0 @@
-//queris for database
-const transactionsAgsAll = 'SELECT * FROM transactions_ags';
-const transactionsTokensAll = 'SELECT * FROM transactions_tokens';
-const accountsOldAll = 'SELECT * FROM az9_claimstatus';
-
-module.exports.transactionsAgsAll = transactionsAgsAll;
-module.exports.transactionsTokensAll = transactionsTokensAll;
-module.exports.accountsOldAll = accountsOldAll;
